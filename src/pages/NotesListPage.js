0132de33b9608878962da918dfe8d38abd620451
Jacob Peat <jacob.peat01@gmail.com>
2022-08-23 14:17:28 +0100

    Create ListItem component have it render note.body from props

--- src/pages/NotesListPage.js
+++ src/pages/NotesListPage.js
@@ -1,4 +1,6 @@
 import React, {useState, useEffect} from 'react'
+import ListItem from '../components/ListItem'
+
 
 const NotesListPage = () => {
   let [notes, setNotes] = useState([])
@@ -19,7 +21,7 @@ const NotesListPage = () => {
     <div>
         <div className='notes-list'>
             {notes.map((note,index)=> (
-                <h3 key={index}>{note.body}</h3>
+                <ListItem key={index} note={note}/>
             ))}
         </div>
     </div>
