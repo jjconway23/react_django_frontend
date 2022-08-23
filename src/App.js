f5cf85f0db90ab688c254d7e83598b964f170567
Jacob Peat <jacob.peat01@gmail.com>
2022-08-23 14:41:34 +0100

    Create routes using react router dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
 import Header from './components/Header'
 import NotesListPage from './pages/NotesListPage';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <NotesListPage/>
-    </div>
+    <Router>
+      <div className="App">
+          <Header/>
+          <Routes>
+            <Route path='/' exact element={<NotesListPage/>} />
+          </Routes>
+          
+      </div>
+    </Router>
+    
   );
 }
 
